fc0be81eb4955560228e8a89e57adebdc532111b
Lauren Groombridge <lauren.groombridge@Nology.local>
2019-05-06 11:27:14 +0100

    initial styling

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -10,7 +10,7 @@ class Header extends React.Component<IProps, IState> {
   public render() {
     return (
       <header className={styles.header}>
-        <h1>hello</h1>
+        <h1>My Library</h1>
       </header>
     );
   }
